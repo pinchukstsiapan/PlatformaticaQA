9acd8899eb3f6cca1115338b2693dc644d4b72fc
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2020-12-01 20:46:12 -0800

    css to xpath

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -8,15 +8,17 @@ import org.testng.annotations.Test;
 public class SimpleTest {
 
     @Test
-    public void simpleTest() {
+    public void simpleTest() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "d:/Work/Projects/chromedriver.exe");
+
         WebDriver browser = new ChromeDriver();
         browser.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
-        WebElement name = browser.findElement(By.cssSelector("div strong a"));
+        WebElement name = browser.findElement(By.xpath("//strong/a"));
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
 
+        Thread.sleep(3000);
         browser.close();
     }
 }
