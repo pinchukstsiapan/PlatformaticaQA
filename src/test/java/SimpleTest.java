3a553f0a56c8f1978eea62fe4bc7f78680fe0b3b
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2020-12-03 18:22:17 -0800

    add BaseTest

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,24 +1,21 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
-public class SimpleTest {
+public class SimpleTest extends BaseTest {
 
     @Test
     public void simpleTest() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "d:/Work/Projects/chromedriver.exe");
-
-        WebDriver browser = new ChromeDriver();
+        WebDriver browser = getDriver();
         browser.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
         WebElement name = browser.findElement(By.xpath("//strong/a"));
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
 
         Thread.sleep(3000);
-        browser.close();
     }
 }
---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,63 @@
+package runner;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class BaseTest {
+
+    public static final String HUB_URL = "http://localhost:4444/wd/hub";
+
+    private static boolean remoteWebDriver = false;
+    static {
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(HUB_URL + "/status").openConnection();
+            try {
+                con.setRequestMethod("GET");
+                remoteWebDriver = con.getResponseCode() == HttpURLConnection.HTTP_OK;
+            } finally {
+                con.disconnect();
+            }
+        } catch (IOException ignore) {}
+
+        if (!remoteWebDriver) {
+            WebDriverManager.chromedriver().setup();
+        }
+    }
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void setUpAll() {
+
+        if (remoteWebDriver) {
+            try {
+                this.driver = new RemoteWebDriver(new URL(HUB_URL), DesiredCapabilities.chrome());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            this.driver = new ChromeDriver();
+        }
+
+        this.driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    protected void setDownAll() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+}
