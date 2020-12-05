060f24849de52f7ec0bf270e2304ab7b102be692
natxs <63980310+natxs@users.noreply.github.com>
2020-12-05 01:22:45 -0600

    added a new class GroupBreakingBad and a new test nataliaVats (#20)
    Co-authored-by: nvatstx <natalia.vats@gmail.com>

---
+++ src/test/java/GroupBreakingBad.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class GroupBreakingBad extends BaseTest {
+    @Test
+    public void nataliaVats() throws InterruptedException {
+
+        WebDriver driver = getDriver();
+        driver.get("https://www.aerotek.com/en/");
+
+        WebElement button = driver.findElement(By.xpath("(//button[contains(text(), 'SEARCH')])[2]"));
+        button.click();
+        Thread.sleep(3000);
+
+        WebElement input = driver.findElement(By.xpath("//input[contains(@class, 'ph-a11y-location-box')]"));
+        Assert.assertEquals(input.getAttribute("placeholder"), "Enter City, State or Zip");
+    }
+}
