ced78c9cdf5a610f46a75debf675d3894d29d164
bogdanmakarenko <makarovbb@gmail.com>
2020-12-08 15:46:24 -0800

    add test BogdanQA

--- src/test/java/GroupJavaBears_Test.java
+++ src/test/java/GroupJavaBears_Test.java
@@ -23,6 +23,7 @@ public class GroupJavaBears_Test extends BaseTest {
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://developer.mozilla.org/en-US/docs/Web/XPath/Functions/contains");
     }
+
     @Test
     public void Alex_Mack() throws InterruptedException {
 
@@ -125,4 +126,15 @@ public class GroupJavaBears_Test extends BaseTest {
         Assert.assertEquals(search_result.getText(), "SEARCH RESULTS");
     }
 
+    @Test
+    public void bogdanQA() throws InterruptedException {
+        WebDriver browser = getDriver();
+        browser.get("https://en.wikipedia.org/wiki/Main_Page");
+
+        WebElement button = browser.findElement(By.xpath("//li[@id='ca-view']/a[contains(text(),'Read')]"));
+        Thread.sleep(3000);
+        Assert.assertEquals(button.getText(), "Read");
+
+        button.click();
+    }
 }
