3340bf34d2d3c4cf40214b294a9ab677305275d0
Tatiana Alexandrova <t021078@gmail.com>
2020-12-08 16:06:20 -0500

    test Tatiana Alexandrova

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
