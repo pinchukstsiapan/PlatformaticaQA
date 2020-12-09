d86f85b0b57f738926a1b388e7655b38265d8332
Tatiana Alexandrova <t021078@gmail.com>
2020-12-09 10:05:30 -0500

    test Tatiana Alexandrova delete Thread sleep and empty row

--- src/test/java/GroupBreakingBad_Test.java
+++ src/test/java/GroupBreakingBad_Test.java
@@ -76,15 +76,14 @@ public class GroupBreakingBad_Test extends BaseTest {
     }
     @Test
     public void testTatyanaAlexandrova () throws InterruptedException {
+
         WebDriver browser = getDriver();
         browser.get("http://selenium.dev");
         WebElement name = browser.findElement(By.id("gsc-i-id1"));
         name.click();
         name.sendKeys("developers", Keys.ENTER);
-        Thread.sleep(2000);
 
         Assert.assertTrue(browser.getPageSource().contains("About"));
-
     }
 
     @Test
