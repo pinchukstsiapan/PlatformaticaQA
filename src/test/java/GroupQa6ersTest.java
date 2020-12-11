3a01440b9de3c46abf0095640660f3fb5242bd7a
Polina <polinabeilina.pb@gmail.com>
2020-12-10 21:44:57 -0500

    add first sample test

--- src/test/java/GroupQa6ersTest.java
+++ src/test/java/GroupQa6ersTest.java
@@ -14,4 +14,20 @@ public class GroupQa6ersTest extends BaseTest {
         WebElement text = driver.findElement(By.xpath("//div/a[@title='Isotopes of selenium']"));
         Assert.assertEquals(text.getText(), "Isotopes of selenium");
     }
+
+    @Test
+    public void polinaBeylina() {
+
+        WebDriver browser = getDriver();
+        browser.get("https://www.performance-lab.ru/");
+
+        WebElement downloadTab = browser.findElement(By.xpath("//a[@href='/blog']"));
+        downloadTab.click();
+
+        WebElement linkToArticle = browser.findElement(By.xpath("//a[@href='https://www.performance-lab.ru/blog/functional-testing/kak-vyyavit-oshibki-sajta-za-1-den']"));
+        linkToArticle.click();
+
+        WebElement title = browser.findElement(By.xpath("//h1[1]"));
+        Assert.assertEquals(title.getText(), "Как выявить ошибки сайта за 1 день?");
+    }
 }
