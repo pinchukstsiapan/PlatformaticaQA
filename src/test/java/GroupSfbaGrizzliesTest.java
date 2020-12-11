ad6bf5736c28aabac9acf21645876d6ca7cbe9fb
LisaJohns <50564933+Kotova9478@users.noreply.github.com>
2020-12-11 00:30:23 -0800

    Lisa j test (#121)
    * add LisaJ_test final for today

--- src/test/java/GroupSfbaGrizzliesTest.java
+++ src/test/java/GroupSfbaGrizzliesTest.java
@@ -11,11 +11,11 @@ public class GroupSfbaGrizzliesTest extends BaseTest {
 
     @Test
     public void klavdiaGoldshteyn() throws InterruptedException {
-       WebDriver browser = getDriver();
-       browser.get("https://en.wikipedia.org/wiki/Roger_Federer");
-       WebElement title = browser.findElement(By.xpath("//h1[@id='firstHeading']"));
+        WebDriver browser = getDriver();
+        browser.get("https://en.wikipedia.org/wiki/Roger_Federer");
+        WebElement title = browser.findElement(By.xpath("//h1[@id='firstHeading']"));
 
-       Assert.assertEquals(title.getText(), "Roger Federer");
+        Assert.assertEquals(title.getText(), "Roger Federer");
     }
 
     @Ignore
@@ -28,7 +28,7 @@ public class GroupSfbaGrizzliesTest extends BaseTest {
         link.click();
         Thread.sleep(2000);
 
-        Assert.assertEquals(browser.getCurrentUrl(),"https://www.elvisthemusic.com/news/");
+        Assert.assertEquals(browser.getCurrentUrl(), "https://www.elvisthemusic.com/news/");
     }
 
     @Ignore
@@ -45,7 +45,7 @@ public class GroupSfbaGrizzliesTest extends BaseTest {
         Thread.sleep(2000);
         WebElement text = driver.findElement(By.xpath("//div//span[3][contains(text(), 'brel')]"));
 
-        Assert.assertEquals(text.getText(),"\"umbrella\"");
+        Assert.assertEquals(text.getText(), "\"umbrella\"");
     }
 
     @Ignore
@@ -70,7 +70,7 @@ public class GroupSfbaGrizzliesTest extends BaseTest {
         WebElement icon = browser.findElement(By.xpath("//input[@id='edit-submit']"));
         icon.click();
 
-        Assert.assertEquals(browser.getCurrentUrl(),"https://www.newyorktheatreguide.com/search?s=Cats");
+        Assert.assertEquals(browser.getCurrentUrl(), "https://www.newyorktheatreguide.com/search?s=Cats");
     }
 
     @Test
@@ -82,5 +82,15 @@ public class GroupSfbaGrizzliesTest extends BaseTest {
         WebElement name = browser.findElement(By.xpath("//a[contains(@id,'thesaurus-nav-tab')]"));
 
         Assert.assertEquals(name.getText(), "THESAURUS.COM");
+    }
+
+    @Test
+    public void lisaJohns() {
+        WebDriver browser = getDriver();
+        browser.get("https://www.rideboreal.com/");
+
+        WebElement title = browser.findElement(By.xpath("//div/h1[text()='HOME AT BOREAL']"));
+
+        Assert.assertEquals(title.getText(), "HOME AT BOREAL");
     }
 }
