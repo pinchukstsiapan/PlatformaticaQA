e01138799885a3623ce45fa0900ce71faaf31f98
Sergey Kuznetsov <votkinsk@gmail.com>
2020-12-10 20:12:34 -0800

    Restore losted tests

--- src/test/java/GroupJavaBearsTest.java
+++ src/test/java/GroupJavaBearsTest.java
@@ -126,6 +126,30 @@ public class GroupJavaBearsTest extends BaseTest {
         Assert.assertEquals(search_result.getText(), "SEARCH RESULTS");
     }
 
+    @Ignore
+    @Test
+    public void olgaV1() {
+
+        WebDriver browser = getDriver();
+        browser.get("https://www.guru99.com/introduction-to-selenium.html");
+
+        WebElement seleniumLink2 = browser.findElement(By.xpath("//a[contains(text(),'Birth of Selenium 2')]"));
+        seleniumLink2.click();
+
+        Assert.assertEquals(browser.getCurrentUrl(), "https://www.guru99.com/introduction-to-selenium.html#8");
+    }
+
+    @Ignore
+    @Test
+    public void olgaV2() {
+
+        WebDriver browser = getDriver();
+        browser.get("https://www.recreation.gov/");
+
+        browser.findElement(By.xpath("//div[@class='nav-links-wrap']/a[1]")).click();
+        Assert.assertEquals(browser.getCurrentUrl(), "https://www.recreation.gov/whats-new");
+    }
+
     @Test
     public void bogdanQA() throws InterruptedException {
         WebDriver browser = getDriver();
@@ -135,7 +159,7 @@ public class GroupJavaBearsTest extends BaseTest {
         Thread.sleep(3000);
         Assert.assertEquals(button.getText(), "Read");
     }
-    
+
     @Test
     public void farizA() {
         WebDriver driver = getDriver();
