d4e64c8fa1f8768610cef50c559608af91fa25a7
ke-de-ve <kedeve@gmail.com>
2020-12-12 15:51:05 -0500

    made a few minor changes to ScreenshotTest

--- src/test/java/ScreenshotTest.java
+++ src/test/java/ScreenshotTest.java
@@ -2,16 +2,19 @@ import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.io.File;
-
+/**
+ * example of how to use TestUtils.makeScreenShot
+ */
 public class ScreenshotTest extends BaseTest {
 
+    /***
+     * demonstration of how to use makeScreenShot within the test
+     */
     @Test
-    public void dmitryTestRun() {
+    public void takeScreenshotDemo() {
 
         WebDriver browser = getDriver();
         browser.get("https://www.wikipedia.org");
@@ -30,9 +33,11 @@ public class ScreenshotTest extends BaseTest {
 
         // imitate failed assert to test makeScreenShotAfterTest method
         //Assert.assertEquals(input.getText(), "Обеспечение качества");
-
     }
 
+    /***
+     * Example of how to take a screenshot after assert fails.
+     */
     @AfterMethod
     public void makeScreenShotAfterTest(ITestResult testResult) {
         if (ITestResult.FAILURE == testResult.getStatus()) {
