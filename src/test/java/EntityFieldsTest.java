96ee447274160c0122e33169bea606b0c6b952f3
StanAretinskiy <sr575sr@gmail.com>
2020-12-12 14:52:20 -0800

    Fix for proper handling low records on Fields

--- src/test/java/EntityFieldsTest.java
+++ src/test/java/EntityFieldsTest.java
@@ -41,9 +41,15 @@ public class EntityFieldsTest extends BaseTest {
 
         // validation of record
         boolean titleFound = false;
+        int rowsPerPage;
         int numOfAllRecords = Integer.parseInt(wait.until(ExpectedConditions.visibilityOfElementLocated(By
                 .xpath("//span[@class='pagination-info']"))).getText().split(" ")[5]);
-        int rowsPerPage = Integer.parseInt(driver.findElement(By.cssSelector("span.page-size")).getText());
+        WebElement rowsPerPageWe = driver.findElement(By.cssSelector("span.page-size"));
+        if (rowsPerPageWe.isDisplayed()) {
+            rowsPerPage = Integer.parseInt(rowsPerPageWe.getText());
+        } else {
+            rowsPerPage = 26;
+        }
         if (numOfAllRecords > rowsPerPage) {
             boolean firstRound = true;
             while (firstRound) {
