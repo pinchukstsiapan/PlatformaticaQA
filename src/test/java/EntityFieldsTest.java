3b1095a86fe97acc94e662161bc0d0bc8713d411
StanAretinskiy <sr575sr@gmail.com>
2020-12-11 18:41:09 -0800

    Delete unused imports

--- src/test/java/EntityFieldsTest.java
+++ src/test/java/EntityFieldsTest.java
@@ -1,16 +1,12 @@
 import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import runner.BaseTest;
-
-import java.util.UUID;
 
 public class EntityFieldsTest extends BaseTest {
 
