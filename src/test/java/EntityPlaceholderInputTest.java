68288a422f76bd25825f323bc725e50c07a969c0
SvetlanaG-cyber <svetlanalegkaya15@gmail.com>
2020-12-14 23:58:30 -0800

    hccts test

--- src/test/java/EntityPlaceholderInputTest.java
+++ src/test/java/EntityPlaceholderInputTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -17,7 +18,6 @@ public class EntityPlaceholderInputTest extends BaseTest {
         driver.get("https://ref.eteam.work");
 
         ProjectUtils.login(driver,"user1@tester.com", "ah1QNmgkEO");
-
         WebElement tab = driver.findElement(By.xpath("//p[contains(text(),'Placeholder')]"));
         tab.click();
 
@@ -110,5 +110,63 @@ public class EntityPlaceholderInputTest extends BaseTest {
         ProjectUtils.click(driver, actions1);
         WebElement delete = driver.findElement(By.xpath(String.format("//tr[@data-index='%s']//div//li/a[text()='delete']", id)));
         ProjectUtils.click(driver, delete);
+    }
+
+    @Test
+    public void deleteRecord() throws InterruptedException {
+
+        final String title = "Delete Placeholder Test";//UUID.randomUUID().toString();
+
+        WebDriver driver = getDriver();
+        driver.get("https://ref.eteam.work");
+
+        ProjectUtils.login(driver,"user1@tester.com", "ah1QNmgkEO");
+
+        WebElement tab = driver.findElement(By.xpath("//p[text()=' Placeholder ']"));
+        Thread.sleep(1000);
+
+        tab.click();
+
+        WebElement createNewFolder = driver.findElement(By.xpath("//i[contains(text(),'create_new_folder')]"));
+        createNewFolder.click();
+
+        WebElement titleElement = driver.findElement(By.xpath("//input[@name='entity_form_data[string]']"));
+        titleElement.sendKeys(title);
+
+        WebElement saveBtn = driver.findElement(By.id("pa-entity-form-save-btn"));
+        ProjectUtils.click(driver,saveBtn);
+
+        WebElement list = driver.findElement(By.xpath("(//div[1]/div/ul/li[1]/a/i)[3]"));
+        list.click();
+
+        WebElement searchBar = driver.findElement(By.xpath("//div[2]/span/input"));
+        searchBar.sendKeys(title);
+
+
+        WebElement testField = driver.findElement(
+                By.xpath("//div[contains(text(),'Delete Placeholder Test')]"));
+
+        Assert.assertEquals(true, testField.isDisplayed());
+
+        Thread.sleep(1000);
+
+        WebElement actionButton = driver.findElement(By.xpath("//table[@id=\"pa-all-entities-table\"]/tbody/tr[1]//button"));
+        actionButton.click();
+
+        Thread.sleep(300);
+        WebElement deleteButton = actionButton.findElement(
+                By.xpath("//tbody/tr[1]//ul[1]/li[3]/a"));
+        deleteButton.click();
+
+        Assert.assertEquals(false, testField.isDisplayed());
+
+        WebElement recycleButton = actionButton.findElement(
+                By.xpath("//i[contains(text(),'delete_outline')]"));
+        recycleButton.click();
+
+        WebElement deletedField = driver.findElement(
+                By.xpath("//b[contains(text(),'Delete Placeholder Test')]"));
+
+        Assert.assertEquals(true, deletedField.isDisplayed());
     }
 }
