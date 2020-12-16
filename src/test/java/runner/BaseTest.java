babeb880d5d7b5c067f436e62907b4221e3fff5d
VictoriaRogo <65843929+VictoriaRogo@users.noreply.github.com>
2020-12-16 15:08:10 -0800

    Add logging (#252)
    * Add logging

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -5,20 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class BaseTest {
 
     public static final String HUB_URL = "http://localhost:4444/wd/hub";
 
+    private static final Logger logger = Logger.getLogger("BaseLogger");
     private static boolean remoteWebDriver = false;
     static {
         try {
@@ -57,11 +61,18 @@ public abstract class BaseTest {
     }
 
     @AfterMethod
-    protected void setDownAll() {
+    protected void setDownAll(Method method, ITestResult tr) {
         driver.quit();
+        long executionTime = (tr.getEndMillis() - tr.getStartMillis()) / 1000;
+        this.log(String.format("\u001B[33m%s.%s() Execution time: %ds\u001B[0m",
+            this.getClass(), method.getName(), executionTime));
     }
 
     protected WebDriver getDriver() {
         return driver;
+    }
+
+    protected void log(String message) {
+        logger.log(Level.INFO, message);
     }
 }
