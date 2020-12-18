772ef3783cb185a0660effeb5e8f8579b3674fbd
Seid31 <bayramov.abuseyit@gmail.com>
2020-12-18 01:23:09 -0500

    Checked edit function on calendar

--- src/test/java/EntityCalendarTest.java
+++ src/test/java/EntityCalendarTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.util.UUID;
 
 public class EntityCalendarTest extends BaseTest {
@@ -46,23 +45,17 @@ public class EntityCalendarTest extends BaseTest {
 
         WebDriver driver = getDriver();
         inputCalendar(driver);
-
         WebElement list = driver.findElement(By.xpath("//div[@class='content']//li[2]"));
         list.click();
-
         WebElement editList = driver.findElement(By.xpath("//button[@class='btn btn-round btn-sm btn-primary dropdown-toggle']"));
         editList.click();
-
         Thread.sleep(1000);
         WebElement clickEdit = driver.findElement(By.xpath("//a[normalize-space()='edit']"));
         clickEdit.click();
-
         WebElement text = driver.findElement(By.xpath("//textarea[@id='text']"));
         text.sendKeys("Ne znayu chto delat");
-
         WebElement number = driver.findElement(By.xpath("//*[@id='int']"));
         number.sendKeys("585");
-
         WebElement save = driver.findElement(By.xpath("//button[normalize-space()='Save']"));
         ProjectUtils.click(driver, save);
 
