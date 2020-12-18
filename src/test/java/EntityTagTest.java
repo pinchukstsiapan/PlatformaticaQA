12c8f31d0d7dfbf74d84bbb73130ab8816bd03cc
OGoncharenko <48034080+OGoncharenko@users.noreply.github.com>
2020-12-18 15:56:06 -0800

    Fix EntityTagTest (#269)
    * Fix EntityTagTest

--- src/test/java/EntityTagTest.java
+++ src/test/java/EntityTagTest.java
@@ -1,9 +1,9 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectUtils;
@@ -12,27 +12,13 @@ import java.util.List;
 
 public class EntityTagTest extends BaseTest {
 
-    private static WebElement tagItem(WebDriver driver) {
-        By tagListItemLocator = By.xpath("//tr[@data-row_id='5']//span[@class='check']");
-        return driver.findElement(tagListItemLocator);
-    }
-
-    private static WebElement assignButton(WebDriver driver) {
-       return driver.findElement(By.xpath("//button[contains(text(), 'Assign tags')]"));
-    }
-
-    private static List<WebElement> assignedTag(WebDriver driver) {
-       return driver.findElements(By.xpath("//tr[@data-row_id='5']//span[@class='pa-tag']"));
-    }
-
-    @Ignore
     @Test
     public void editTest() {
 
         WebDriver driver = getDriver();
         driver.get("https://ref.eteam.work");
 
-        ProjectUtils.login(driver, "user1@tester.com", "ah1QNmgkEO");
+        ProjectUtils.loginProcedure(driver);
 
         WebElement sidebar = driver.findElement(By.xpath("//div[contains(@class, 'sidebar-wrapper')]"));
         Actions builder = new Actions(driver);
@@ -40,20 +26,69 @@ public class EntityTagTest extends BaseTest {
         WebElement tag = driver.findElement(By.xpath("//li/a/p[contains(text(), 'Tag')]"));
         builder.moveToElement(tag).build().perform();
         tag.click();
-        By tagIconLocator = By.xpath("//li/a[contains(@href, 'index.php?action=action_list&list_type=table&entity_id=42&taggable=1')]");
 
+        String tagEntityText = createTagEntity(driver);
+        String newTagText = createTag(driver);
+
+        tagEntity(driver, tagEntityText).click();
+
+        By newTagLocator = By.xpath("//label[contains(text(), '" + newTagText + "')]");
+        WebElement newTag = driver.findElement(newTagLocator);
+        builder.moveToElement(newTag).perform();
+        newTag.click();
+        ProjectUtils.click(driver, assignButton(driver, builder));
+
+        Assert.assertEquals(assignedTag(driver, newTagText).size(), 1);
+        tagEntity(driver, tagEntityText).click();
+        ProjectUtils.click(driver, assignButton(driver, builder));
+        Assert.assertEquals(assignedTag(driver, newTagText).size(), 0);
+    }
+
+    private static WebElement assignButton(WebDriver driver, Actions builder) {
+        WebElement assignButton =  driver.findElement(By.xpath("//button[@value='assign']"));
+        builder.moveByOffset(10,261).moveToElement(assignButton).perform();
+        return assignButton;
+    }
+
+    private static List<WebElement> assignedTag(WebDriver driver, String newTagText) {
+
+        By assignedTagLocator = By.xpath("//span[@class='pa-tag'][contains(text(), '" + newTagText + "')]");
+        return driver.findElements(assignedTagLocator);
+    }
+
+    private static String createTagEntity(WebDriver driver) {
+        By createTagIconLocator = By.xpath("//div[@class='card-icon']");
+        WebElement createTagIcon = driver.findElement(createTagIconLocator);
+        createTagIcon.click();
+        WebElement stringInput = driver.findElement(By.xpath("//input[@id='string']"));
+        String randomTag = RandomStringUtils.randomNumeric(10);
+        stringInput.sendKeys(randomTag);
+        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
+        saveButton.click();
+        return randomTag;
+    }
+
+    private static String createTag(WebDriver driver) {
+        By tagIconLocator = By.xpath("//li/a[contains(@href, 'index.php?action=action_list&list_type=table&entity_id=42&taggable=1')]");
         WebElement tagIcon = driver.findElement(tagIconLocator);
         tagIcon.click();
 
-        By tagCheckboxLocator = By.xpath("//div/label[@class='form-check-label'][contains(text(), 'tag for view')]");
-        WebElement tagCheckbox = driver.findElement(tagCheckboxLocator);
-        tagCheckbox.click();
-        tagItem(driver).click();
+        By newTagInputLocator = By.xpath("//input[@name='new_tag']");
+        WebElement newTagInput = driver.findElement(newTagInputLocator);
+        By newTagButtonLocator = By.xpath("//button[contains(text(), 'New tag')]");
+        WebElement newTagButton = driver.findElement(newTagButtonLocator);
 
-        assignButton(driver).click();
-        Assert.assertEquals(assignedTag(driver).get(0).getText(), "tag for view");
-        tagItem(driver).click();
-        assignButton(driver).click();
-        Assert.assertEquals(assignedTag(driver).size(), 0);
+        Actions builder = new Actions(driver);
+        builder.moveToElement(newTagButton).perform();
+
+        String randomTag = RandomStringUtils.randomNumeric(5);
+        newTagInput.sendKeys(randomTag);
+        newTagButton.click();
+        return randomTag;
+    }
+
+    private static WebElement tagEntity(WebDriver driver, String tagEntityText) {
+        By tagEntityLocator = By.xpath("//tr//td/a/div[contains(text(), '" + tagEntityText + "')]/../../../td[1]/div");
+        return driver.findElement(tagEntityLocator);
     }
 }
