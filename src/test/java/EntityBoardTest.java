754db2f80be719188231fc17a350e79f9877fe77
Dmitry Tregubov <63436344+4stuntman4@users.noreply.github.com>
2020-12-22 07:37:09 -0800

    EnityBoardTest_Refactor (#349)
    * added image file to upload

--- src/test/java/EntityBoardTest.java
+++ src/test/java/EntityBoardTest.java
@@ -1,27 +1,24 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectUtils;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.UUID;
 
 public class EntityBoardTest extends BaseTest {
 
     @Test
     public void inputTest() throws InterruptedException {
-
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        String currentDataEuropean = formatter.format(calendar.getTime());
         final String text = UUID.randomUUID().toString();
-        final int number = 12;
-        final double decimal = 10.25;
+        final int number = (int) (Math.random()*100);
+        final double decimal = (int) (Math.random()*20000) / 100.0;
+        final String dateField = "05/09/1945";
+        final String dateTimeField = "05/09/1945 12:34:56";
         final String pending = "Pending";
-        final String user1Demo = "User 1 Demo";
+        final String userDemo = "user249@tester.com";
 
         WebDriver driver = getDriver();
         WebDriverWait wait = new WebDriverWait(driver,30);
@@ -36,11 +33,9 @@ public class EntityBoardTest extends BaseTest {
         createNew.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name='entity_form_data[text]']")));
 
-        WebElement stringDropdown = driver.findElement(By.xpath("//div[text()= 'Pending']/.."));
-        stringDropdown.click();
-
-        WebElement stringPending = driver.findElement(By.xpath("//div[text()= 'Pending']"));
-        stringPending.click();
+        WebElement dropdownStatus = driver.findElement(By.id("string"));
+        Select s = new Select(dropdownStatus);
+        s.selectByVisibleText(pending);
 
         WebElement textPlaceholder = driver.findElement(By.xpath("//textarea[@name='entity_form_data[text]']"));
         ProjectUtils.sendKeys(textPlaceholder, text);
@@ -56,38 +51,34 @@ public class EntityBoardTest extends BaseTest {
         wait.until(ExpectedConditions.attributeContains(decimalPlaceholder, "value", String.valueOf(decimal)));
 
         WebElement date = driver.findElement(By.id("date"));
-        date.click();
+        ProjectUtils.setAttribute(driver, date, "value", dateField);
 
         WebElement dateTime = driver.findElement(By.id("datetime"));
-        dateTime.click();
+        ProjectUtils.setAttribute(driver, dateTime, "value", dateTimeField);
+
+        WebElement dropdownUser = driver.findElement(By.id("user"));
+        Select s2 = new Select(dropdownUser);
+        s2.selectByVisibleText(userDemo);
 
         WebElement saveBtn = driver.findElement(By.id("pa-entity-form-save-btn"));
         ProjectUtils.click(driver, saveBtn);
 
         String recordTitleXpath = String.format("//div[contains(text(), '%s')]", text);
-        By newRecordStringPending = By.xpath(String.format("%s/../../../td[2]/a/div", recordTitleXpath));
-        By newRecordInt = By.xpath(String.format("%s/../../../td[4]/a/div", recordTitleXpath));
-        By newRecordDecimal = By.xpath(String.format("%s/../../../td[5]/a/div", recordTitleXpath));
-        By newRecordDate = By.xpath(String.format("%s/../../../td[6]/a/div", recordTitleXpath));
-        By newRecordDateTime = By.xpath(String.format("%s/../../../td[7]/a/div", recordTitleXpath));
-        By newRecordUser1Demo = By.xpath(String.format("%s/../../../td[9]", recordTitleXpath));
+        WebElement createdRecordText = driver.findElement(By.xpath(recordTitleXpath));
+        WebElement createdRecordStringPending = driver.findElement( By.xpath(String.format("%s/../../../td[2]/a/div", recordTitleXpath)));
+        WebElement createdRecordInt = driver.findElement(By.xpath(String.format("%s/../../../td[4]/a/div", recordTitleXpath)));
+        WebElement createdRecordDecimal = driver.findElement(By.xpath(String.format("%s/../../../td[5]/a/div", recordTitleXpath)));
+        WebElement createdRecordDate = driver.findElement(By.xpath(String.format("%s/../../../td[6]/a/div", recordTitleXpath)));
+        WebElement createdRecordDateTime = driver.findElement(By.xpath(String.format("%s/../../../td[7]/a/div", recordTitleXpath)));
+        WebElement createdNewImage = driver.findElement(By.xpath(String.format("%s/../../../td[8]", recordTitleXpath)));
+        WebElement createdRecordUserDemo = driver.findElement(By.xpath(String.format("%s/../../../td[9]", recordTitleXpath)));
 
-        By createdRecordText = By.xpath(recordTitleXpath);
-        WebElement newCreatedRecordText = wait.until(ExpectedConditions.visibilityOfElementLocated(createdRecordText));
-
-        WebElement createdRecordStringPending = driver.findElement(newRecordStringPending);
-        WebElement createdRecordInt = driver.findElement(newRecordInt);
-        WebElement createdRecordDecimal = driver.findElement(newRecordDecimal);
-        WebElement createdRecordDate = driver.findElement(newRecordDate);
-        WebElement createdRecordDateTime = driver.findElement(newRecordDateTime);
-        WebElement createdRecordUser1Demo = driver.findElement(newRecordUser1Demo);
-
-        Assert.assertEquals(newCreatedRecordText.getText(), text, "Created record text issue");
         Assert.assertEquals(createdRecordStringPending.getText(), pending, "Created record Pending issue");
+        Assert.assertEquals(createdRecordText.getText(), text, "Created record text issue");
         Assert.assertEquals(createdRecordInt.getText(), Integer.toString(number), "Created record number issue");
         Assert.assertEquals(createdRecordDecimal.getText(), Double.toString(decimal), "Created record decimal issue");
-        Assert.assertEquals(createdRecordDate.getText(), currentDataEuropean, "Created date issue");
-        Assert.assertEquals(createdRecordUser1Demo.getText(), user1Demo, "Created user issue");
+        Assert.assertEquals(createdRecordDate.getText(), dateField, "Created date issue");
+        Assert.assertEquals(createdRecordDateTime.getText(), dateTimeField, "Created dateTime issue");
+        Assert.assertEquals(createdRecordUserDemo.getText(), userDemo, "Created user issue");
     }
-
 }
