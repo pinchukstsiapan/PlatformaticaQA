55bab1d2a449434fcf561f7e0d1073295f1d4843
irina0606 <irzhik1975@gmail.com>
2020-12-23 20:24:26 -0500

    code improvement

--- src/test/java/EntityBoardTest.java
+++ src/test/java/EntityBoardTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.UUID;
 
 public class EntityBoardTest extends BaseTest {
@@ -145,4 +148,92 @@ public class EntityBoardTest extends BaseTest {
                 "//div[contains(text(), 'Good job with housekeeping! Recycle bin is currently empty!')]"));
         Assert.assertNotNull(emptyRecycleBin, "No empty recycle bin message found.");
     }
+
+    @Test
+    public void editBoard() throws InterruptedException {
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String currentDataEuropean = formatter.format(calendar.getTime());
+        final String id = UUID.randomUUID().toString();
+
+        WebDriver driver = getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement board = driver.findElement(By.xpath("//p[contains(text(),'Board')]"));
+        ProjectUtils.click(driver, board);
+
+        WebElement newFolder = driver.findElement(By.xpath("//i[contains(text(),'create_new_folder')]"));
+        newFolder.click();
+
+        WebElement text = driver.findElement(By.xpath("//textarea[@id='text']"));
+        text.sendKeys(id);
+
+        WebElement integer = driver.findElement(By.xpath("//input[@id='int']"));
+        integer.sendKeys(String.valueOf(20));
+
+        WebElement decimal = driver.findElement(By.xpath("//input[@id='decimal']"));
+        decimal.sendKeys(String.valueOf(22.5));
+
+        WebElement date = driver.findElement(By.id("date"));
+        date.click();
+
+        WebElement dateTime = driver.findElement(By.id("datetime"));
+        dateTime.click();
+
+        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
+        ProjectUtils.click(driver, saveButton);
+
+        WebElement dashboard = driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='dashboard']"));
+        dashboard.click();
+
+        WebElement list = driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='list']"));
+        list.click();
+
+        WebElement container = driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]"));
+        container.click();
+
+        WebElement edit = driver.findElement(By.xpath("//a[text()='edit']"));
+        ProjectUtils.click(driver, edit);
+
+        WebElement pending = driver.findElement(By.xpath("//div[contains(text(),'Pending')]"));
+        pending.click();
+        wait.until(ExpectedConditions.elementToBeClickable(pending));
+
+        WebElement onTrack = driver.findElement(By.xpath("//span[contains(text(),'On track')]"));
+        onTrack.click();
+
+        WebElement newText= driver.findElement(By.xpath("//textarea[@id='text']"));
+        newText.clear();
+        newText.sendKeys("my test changed");
+
+        WebElement newInteger = driver.findElement(By.xpath("//input[@id='int']"));
+        newInteger.clear();
+        newInteger.sendKeys(String.valueOf(50));
+
+        WebElement newDecimal = driver.findElement(By.xpath("//input[@id='decimal']"));
+        newDecimal.clear();
+        newDecimal.sendKeys(String.valueOf(50.5));
+
+        WebElement saveButton1 = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
+        ProjectUtils.click(driver, saveButton1);
+
+        String result = driver.findElement(By.xpath("//tbody/tr[1]/td[3]/a[1]/div[1]")).getText();
+        Assert.assertEquals(result, "my test changed");
+
+        WebElement container1 = driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]"));
+        container1.click();
+        wait.until(ExpectedConditions.elementToBeClickable(container1));
+
+        WebElement delete = driver.findElement(By.xpath("//a[text()='delete']"));
+        delete.click();
+
+        driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='list']")).click();
+        boolean emptyField = driver.findElements(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]")).size() < 1;
+        Assert.assertTrue(emptyField);
+    }
 }
+
+
+
+
