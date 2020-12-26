dcac483e59473f6ffe5b3826bd1b8cb28f85e043
StanAretinskiy <sr575sr@gmail.com>
2020-12-26 13:59:00 -0800

    Fix attempt #2 EntityBoardTest.editBoard

--- src/test/java/EntityBoardTest.java
+++ src/test/java/EntityBoardTest.java
@@ -67,7 +67,7 @@ public class EntityBoardTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = "inputValidationTest")
-    public void editBoard() {
+    public void editBoard() throws InterruptedException {
 
         WebDriver driver = getDriver();
         WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -94,8 +94,7 @@ public class EntityBoardTest extends BaseTest {
 
         WebElement text1 = driver.findElement(By.id("text"));
         text1.clear();
-        text1.sendKeys("my test changed");
-        new WebDriverWait(driver, 2).until(d -> text1.getText().equals("my test changed"));
+        ProjectUtils.sendKeys(text1, "my test changed");
 
         WebElement integer1 = driver.findElement(By.id("int"));
         integer1.clear();
