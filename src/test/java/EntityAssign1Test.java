cfafc3cd61edd4071d79eba5be352d59a1e93156
MaxBurdin <maxburdin1@gmail.com>
2021-01-07 19:18:13 -0800

    - added last verification of editTest which tests visibility of reassigned record under another account https://trello.com/c/AeIJ2vvl/319-entity-assign-edit-copy-at - ignored this test because of the bug https://trello.com/c/wZ6roVDO/36-entity-assign-edit-re-assigned-task-does-not-appear-in-the-user-list - replaced string concatenations with format

--- src/test/java/EntityAssign1Test.java
+++ src/test/java/EntityAssign1Test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectUtils;
@@ -12,6 +13,11 @@ import runner.type.ProfileType;
 import runner.type.Run;
 import runner.type.RunType;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -35,6 +41,39 @@ public class EntityAssign1Test extends BaseTest {
         driver.navigate().refresh();
     }
 
+    private String[] getAnotherUserCreds() {
+
+        int counter = 0;
+
+        while (counter < 11) {
+            counter++;
+            try {
+                HttpURLConnection con = (HttpURLConnection) new URL("https://ref.eteam.work/next_tester.php").openConnection();
+                con.setRequestMethod("GET");
+                if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String response = in.readLine();
+                    String[] responseArray = response.split(";");
+                    if (!responseArray[0].equals(ProfileType.DEFAULT.getUserName())) {
+                        con.disconnect();
+                        return responseArray;
+                    }
+                }
+                con.disconnect();
+            } catch (IOException ignore) {}
+        }
+        throw new RuntimeException("Unable to get credentials for another account");
+    }
+
+    private void authentication(WebDriver driver, String username, String password) throws InterruptedException {
+
+        ProjectUtils.click(driver, driver.findElement(By.xpath("//a[text()='Log out']")));
+        Thread.sleep(400);
+        driver.findElement(By.xpath("//input[@name='login_name']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[text()='Sign in']")).click();
+    }
+
     @Test
     public void assignTest() {
 
@@ -50,13 +89,13 @@ public class EntityAssign1Test extends BaseTest {
         driver.findElement(By.xpath("//input[@id='datetime']")).click();
         ProjectUtils.click(driver, driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")));
         WebElement userSelect = driver.findElement(By.xpath
-                ("//option[text()='" + ProfileType.DEFAULT.getUserName() + "']"));
+                (String.format("//option[text()='%s']", ProfileType.DEFAULT.getUserName())));
         userSelect.click();
 
         driver.navigate().refresh();
 
         WebElement selectedUser = driver.findElement(By.xpath
-                ("//option[@selected and text()='" + ProfileType.DEFAULT.getUserName() + "']"));
+                (String.format("//option[@selected and text()='%s']", ProfileType.DEFAULT.getUserName())));
 
         Assert.assertTrue(selectedUser.isDisplayed());
 
@@ -66,16 +105,20 @@ public class EntityAssign1Test extends BaseTest {
         Assert.assertTrue(driver.findElement(By.xpath("//tbody/tr")).isDisplayed());
     }
 
+    @Ignore
     @Test (dependsOnMethods = "assignTest")
-    public void editTest() {
+    public void editTest() throws InterruptedException {
 
-        String anotherUser = "user" + (int)(Math.random() * (249 - 100 + 1) + 100) + "@tester.com";
+        String[] anotherUserCreds = getAnotherUserCreds();
+        String anotherUserLogin = anotherUserCreds[0];
+        String anotherUserPassword = anotherUserCreds[1];
 
         WebDriver driver = getDriver();
 
-        changeUser(driver, anotherUser);
+        changeUser(driver, anotherUserLogin);
 
-        WebElement selectedUser = driver.findElement(By.xpath("//option[@selected and text()='" + anotherUser + "']"));
+        WebElement selectedUser = driver.findElement(By.xpath
+                (String.format("//option[@selected and text()='%s']", anotherUserLogin)));
 
         Assert.assertTrue(selectedUser.isDisplayed());
 
@@ -85,12 +128,22 @@ public class EntityAssign1Test extends BaseTest {
         getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         Assert.assertTrue(driver.findElements(By.xpath("//tbody/tr")).isEmpty());
         getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        authentication(driver, anotherUserLogin, anotherUserPassword);
+
+        driver.findElement(By.xpath("//li[@id='pa-menu-item-41']")).click();
+
+        getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.findElement(By.xpath("//tbody/tr")).isDisplayed());
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test (dependsOnMethods = "editTest")
-    public void deleteTest() {
+    public void deleteTest() throws InterruptedException {
 
         WebDriver driver = getDriver();
+
+        authentication(driver, ProfileType.DEFAULT.getUserName(), ProfileType.DEFAULT.getPassword());
 
         changeUser(driver, ProfileType.DEFAULT.getUserName());
 
