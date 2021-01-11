b4e8289a682a7d1a86cc466e4d18805bed360cb8
oooleks <oooleks@gmail.com>
2021-01-11 12:54:47 -0600

    EntityLoop1Test - refactoring

--- src/test/java/EntityLoop1Test.java
+++ src/test/java/EntityLoop1Test.java
@@ -11,17 +11,17 @@ import runner.ProjectUtils;
 
 public class EntityLoop1Test extends BaseTest {
 
-    private WebDriverWait getWait(int timeoutSecond) {
-        return new WebDriverWait(getDriver(), timeoutSecond);
+    private static WebDriverWait getWait200(WebDriver driver) {
+        return new WebDriverWait(driver, 200);
     }
 
-    private final By actions_button = By.xpath("//tr[@data-index='0']/td/div/button");
+    private static final By ACTIONS_BUTTON = By.xpath("//tr[@data-index='0']/td/div/button");
 
     private void assertLoopValues(WebDriver driver, int value, String mode) {
-        getWait(1).until(ExpectedConditions.visibilityOfElementLocated(actions_button)).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(ACTIONS_BUTTON)).click();
         if (mode.equals("view")) {
-            getWait(1).until(ExpectedConditions.visibilityOfElementLocated(By.linkText(mode))).click();
-            int f1 = Integer.parseInt(getWait(1).until(ExpectedConditions.visibilityOfElementLocated
+            ProjectUtils.click(driver, driver.findElement(By.linkText(mode)));
+            int f1 = Integer.parseInt(getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated
                     (By.xpath("//label[(text()='F1')]/following-sibling::div/child::div/child::span"))).getText());
             Assert.assertEquals(f1, value);
 
@@ -37,8 +37,8 @@ public class EntityLoop1Test extends BaseTest {
             driver.navigate().back();
         } else {
             if (mode.equals("edit")) {
-                getWait(1).until(ExpectedConditions.visibilityOfElementLocated(By.linkText(mode))).click();
-                int f1 = Integer.parseInt(getWait(1).until(ExpectedConditions.visibilityOfElementLocated
+                ProjectUtils.click(driver, driver.findElement(By.linkText(mode)));
+                int f1 = Integer.parseInt(getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated
                         (By.xpath("//div[@id='_field_container-f1']/child::span/child::input"))).getAttribute("value"));
                 Assert.assertEquals(f1, value);
 
@@ -51,7 +51,7 @@ public class EntityLoop1Test extends BaseTest {
                         (By.xpath("//div[@id='_field_container-f3']/child::span/child::input")).getAttribute("value"));
                 value += 1;
                 Assert.assertEquals(f3, value);
-                ProjectUtils.click(driver, driver.findElement(By.xpath("//button[text()='Cancel']/parent::a")));
+                ProjectUtils.click(driver, driver.findElement(By.xpath("//button[contains(text(),'Cancel')]/parent::a")));
             } else {
                 Assert.fail("Wrong mode provided to assert loop values (accepted values: view or edit)");
             }
@@ -59,8 +59,7 @@ public class EntityLoop1Test extends BaseTest {
     }
     @Ignore("The test is good, but too long. Let it be ignored until we switch to parallel execution")
     @Test
-    public void loop1Stops() {
-
+    public void loop1Stops() throws InterruptedException {
         WebDriver driver = getDriver();
 
         WebElement loop_1 = driver.findElement(By.xpath("//p[contains(text(),'Loop 1')]"));
@@ -69,33 +68,37 @@ public class EntityLoop1Test extends BaseTest {
         final int number_1 = 1;
         final int number_2 = 0;
 
-        WebElement new_loop = driver.findElement(By.xpath("//i[text()='create_new_folder']"));
+        WebElement new_loop = driver.findElement(By.xpath("//i[contains(text(),'create_new_folder')]"));
         new_loop.click();
 
         WebElement f1_element = driver.findElement(By.xpath("//div[@id='_field_container-f1']/child::span/child::input"));
         WebElement f3_element = driver.findElement(By.xpath("//div[@id='_field_container-f3']/child::span/child::input"));
-        f1_element.sendKeys(String.valueOf(number_1));
+        ProjectUtils.inputKeys(driver, f1_element, number_1);
 
-        getWait(200).until(ExpectedConditions.attributeContains(f3_element, "value", "1002"));
+        getWait200(driver).until(ExpectedConditions.attributeContains(f3_element, "value", "1002"));
         Assert.assertEquals(f1_element.getAttribute("value"),"1000");
 
         ProjectUtils.click(driver, driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")));
         assertLoopValues(driver, 1000, "view");
 
-        getWait(1).until(ExpectedConditions.visibilityOfElementLocated(actions_button)).click();
-        getWait(1).until(ExpectedConditions.visibilityOfElementLocated(By.linkText("edit"))).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(ACTIONS_BUTTON)).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.linkText("edit"))).click();
         WebElement f1_edit = driver.findElement(By.xpath("//div[@id='_field_container-f1']/child::span/child::input"));
         WebElement f3_edit = driver.findElement(By.xpath("//div[@id='_field_container-f3']/child::span/child::input"));
-        getWait(1).until(ExpectedConditions.visibilityOf(f1_edit)).clear();
-        f1_edit.sendKeys(String.valueOf(number_2));
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(f1_edit)).clear();
+        ProjectUtils.inputKeys(driver, f1_edit, number_2);
 
-        getWait(200).until(ExpectedConditions.attributeContains(f3_edit, "value", "1001"));
+        getWait200(driver).until(ExpectedConditions.attributeContains(f3_edit, "value", "1001"));
         ProjectUtils.click(driver, driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")));
 
         assertLoopValues(driver, 999, "view");
         assertLoopValues(driver, 999, "edit");
 
-        getWait(1).until(ExpectedConditions.visibilityOfElementLocated(actions_button)).click();
+        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(ACTIONS_BUTTON)).click();
         driver.findElement(By.linkText("delete")).click();
+
+        WebElement loop1_table = getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated
+                (By.xpath("//div[contains(@class,'card-body')]")));
+        Assert.assertTrue(loop1_table.getText().isEmpty());
     }
 }
